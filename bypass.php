9fc229359e3fb1e5247d44b469e83a05393492de
shivanggupta1509 <87229803+shivanggupta1509@users.noreply.github.com>
2022-05-26 13:10:52 +0530

    Add files via upload

---
+++ bypass.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Shivang Gupta</title>
+	<meta name="viewport" content="width=device-width">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+</head>
+<style>
+
+body{ font-family:Oleo Script; background:black; //background:radial-gradient(white,purple); } div { margin: 5px; background-color: none; border: solid; color: black; padding: 15px 32px; text-align: center; }hr { overflow: visible; /* For IE */ height: 3px; border-style: solid; border-color: blue; border-width: 2px 0 0 0; border-radius: 2px; }hr:before { /* Not really supposed to work, but does */ display: block; content: ""; height: 3px; border-style: solid; border-color: blue; border-width: 0 0 2px 0; border-radius: 2px; }.script {color:blue;text-decoration:none; } .script:hover{color:green;text-decoration:none;} table{ width:100%px; table-layout: fixed;}.tbl-header{ background-color: rgba(255,255,255,0.3); }.tbl-content{ height:300px; overflow-x:auto; margin-top: 0px; border: 2px solid rgba(255,255,255,0.3);}th{ padding: 20px 15px; text-align: left; font-weight: 500; font-size: 15px; color: darkred; }td{ padding: 15px; text-align: left; vertical-align:middle; font-weight: 300; font-size: 17px; color: #fff; border-bottom: solid 1px rgba(255,255,255,0.1);} /* demo styles */ @import url(https://fonts.googleapis.com/css?family=Roboto:400,500,300,700);body{ background: -webkit-linear-gradient(left, #25c481, #25b7c4); background: linear-gradient(to right, #25c481, #25b7c4); font-family: Oleo Script;}section{ margin: 10px;} /* for custom scrollbar for webkit browser*/ ::-webkit-scrollbar { width: 18px;} ::-webkit-scrollbar-track { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } ::-webkit-scrollbar-thumb { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } input, textarea, select { display: inline-block; -webkit-box-sizing: content-box; -moz-box-sizing: content-box; box-sizing: content-box; padding: 10px 20px; border: 1px solid #b7b7b7; -webkit-border-radius: 13px / 21px; border-radius: 13px / 21px; font: normal 16px/normal "Comic Sans MS", cursive, sans-serif; color: rgba(0,142,198,1); text-align: center; -o-text-overflow: clip; text-overflow: clip; word-spacing: 1px; -webkit-box-shadow: 1px 0 12px 5px rgba(58,110,201,0.2) ; box-shadow: 1px 0 12px 5px rgba(58,110,201,0.2) ; -webkit-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); -moz-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); -o-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); min-width: 20%; } input[type=text], select { width: 80%; background-color: white;font-family: normal;color: rgba(0,142,198,1); font-weight:normail; padding: 12px 20px; margin: 5px 0; display: inline-block; border: 2px solid #00aaff; border-radius: 7px; box-shadow: 1px 4px 3px #aaa; box-sizing: border-box;} input[type=text]:hover {width: 85%; background-color:white;border: 2px  solid black;} fieldset { padding:1px 2px; } input[type="submit"] {color: white; font-weight: bold;width: 40%; padding: 13px 16px; background: #00aaff; border: none;} input[type=submit]:hover { 	background-color : #262626; } input, textarea, select { background: #f4f4f4; width: 70%; margin-top: 2px; font-family: Alura;margin-bottom: 2px; font: ; border: 1px solid #00aaff; border-radius: 9px; padding: 13px 16px; color: #000;}.head{text-shadow: #e6daac 1px 1px;color: #444;font-size: 18px;font-weight: bold; padding: 3px;border-bottom: 1px solid #edaa09; background: #fff2bf; box-shadow: 1px 2px 3px #aaa;}.header{color: #fff;font-weight: bold; padding: 1px 2px; border: 2px solid red; background: #fff;background-img: url(http://css-style.xtgem.com/image/bmenu.gif) repeat-x top; box-shadow: 1px 1px 2px #aaa;border-radius: 5px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;}
+.title{color: #fff;font-weight: bold; padding: 20px; }.error {background:#ffebe8; border:1px solid #dd3c10; padding:2px; text-align:center;font-weight:bold; padding: 8px 10px;color:#000; border-radius: 7px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;} .success {background:#fff8cc; border:2px solid #ffe222; padding:10px; text-align:center; font-weight:bold; padding: 8px 10px;color:#000;border-radius: 7px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;}.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }.spd{background-color: #000; background-img: url() no-repeat; padding: 5px 7px; margin: 2px; border: 1px dotted darkred;}.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; } Zig { 	border-radius : 5px; 	background-color : #0AFF0A; 	padding : 20px; } div { 	border-radius : 5px; 	background-color :lighterd; 	padding : 20px;border:2px solid blue; padding:20px; color:#000; background-img: url() no-repeat; padding: 5px 7px; margin: 2px; }.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }.DV{ 	background:#ffebe8; border:1px solid #dd3c10; padding:2px; text-align:center; font-weight:normal; color:maroon; } body{background:linear-gradient( to right black 100%); font-family:Oleo Script; color:black; opacity:100%; }.non{display:none;},
+
+</style>
+<body><center><font size='5'><b>Quixo REFER SCRIPT </b></font></center></div>
+<fieldset style='border: 1px  ;'><center><font color='#000000'><b>Shivang Gupta</b></font><hr>
+<font color='#000' size='px'></font></center></fieldset>
+<center>
+ 
+      <iframe allowtransparency="true" scrolling="no" frameborder="0" framespacing="0" width="320" height="10" src="about:blank"></iframe> 
+     </center> 
+
+<?php
+function RandomNumber($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+
+$m=array("ram","shyam","lalit","som","sonu","king","nemar","nandu","lokesh","rahul","tom","tony","michal","jones","starc","smith","smriti","suman","pooja","jyoti","munro","guptil","taylor","tanu","tejas","tanmay","surbi" ,"gaurav","goru","ganesh","decock","amla","anju","hero","killer","russel","simmons","sindhu","hacker", "sambhu","sabita","chinu","donald","kapil","viru","baldau","ramu","rawdi","ravan","ram2","shyam2","lalit2","somw","sronu","kinhg","nemhar","nanbdu","lokhesh","rahhul","tohm","tonhy","michhal","jonjes","starhch","smijth","smrhiti","sumhan","poongja","jyhoti","munbro","gupktibl","taylbdjdjor","tajdudnhu","tejajdjdhs","tanmhajdjdy","surgeiurjdbi" ,"gaurhdjdjjdfjav","gojrdjdjjrjru","ganehudjdsh","dechoudjdck","amudjdjdlga","anididju","hedujdro","killedhdur","russeduudl","simmojdjdns","sijdjdndhu","hacdjdjker", "samjdjdbhu","sabidjjdta","chijdjdnu","donjdjdald","kapdjdjil","virdjjdu","baldadjjdu","rajdjdmu","rawddjjdi","ravdjjdan");
+$fname=$m[mt_rand(0,50)];
+
+$n=array("patjdjdel","shardjdjma","sinhdudgh","thakfjjdur","gupududta","kumari","kundu","kumari","soni","trumph","steve","som","mondal","mick","manav","maryin","jones","hamza","rathour","mondak","kohli","dhawan","silva","dhoni","patil","poker","pronjal","piku","pondu","randi","rawdi","jalbbi","julmi","joker","khan","chaubey","ali","vishnoi","voni","ballad","billu","kalia","kuber","roberg","robert","ren","rewsi","modu",);
+$lname=$n[mt_rand(0,50)];
+
+$bn=array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+$fme=$bn[mt_rand(0,50)];
+$bne=array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+$fmee=$bne[mt_rand(0,50)];
+
+
+
+
+
+
+$sname="$fname$lname";
+$k=RandomNumber(7);
+$kv=RandomNumber(4);
+$l=RandomNumber(2);
+$kq=RandomNumber(3);
+$g=RandomNumber(21);
+$abc=RandomNumber(27);
+$fcmee="$fme$l$fmee0$fme$g$$fmee$g";
+
+ $reff=$_GET['refer'] ;
+
+ $emali="$sname$k%40gmail.com";
+ $coin="$co$cos";
+
+$url1='http://aws.ielite.xyz/api/registration';
+$data1='email='.$emali.'&password=iitbombaycsw&first_name='.$sname.'&mobile_number=99'.$k.'4&device_token=ddDyMwTUTPKEiq45D2Z18B%3AAPA91bFxJAKxqfcz_GClzykODCd7fTfAIe2pkGjCv0cSwYucA6M4SMDliDY_vsPaHGmtBdSY1Ee9j75E9ktiekDIpO-iosZFqjmDKSPae1'.$k.'R-rDR69aLQy0YPFATUqunvBJUs-&parent_reference_code='.$reff.'';
+
+$headers1[]='Host: aws.ielite.xyz';
+$headers1[]='Connection: keep-alive';
+$headers1[]='Content-Length: '.strlen($data1).'';
+$headers1[]='okhttp/3.14.9';
+$headers1[]='application/x-www-form-urlencoded';
+
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_URL,$url1);
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$data1);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers1);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+    $output=curl_exec($ch);
+    curl_close($ch);
+    
+
+echo "<font color='red' size='6'>$output</font>";
+?> 	 <meta http-equiv="refresh"
+ content= "4" >
+
+
+<center><script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//telegram.im/widget-button/index.php?id=@KrishnaEarningZone";document.getElementsByTagName("head")[0].appendChild(script);})();</script> <a href="tg://resolve?domain=KrishnaEarningZone" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:18px;max-width:300px;background: linear-gradient(20deg,  #40bfbf 30%, #ff0000 50%,#0066ff 30%
+ );text-transform:uppercase;background-size: 400% 400%; animation: gradient 2s ease infinite; } @keyframes gradient { 0% { background-position: 0% 50%; } 50% { background-position: 100% 50%; } 100% { background-position: 0% 50%; }}
+hr{
+    border:0;
+    height:1px;
+    background-image:linear-gradient(to right,rgba(0,0,0,0),rgba(0,255,0,1),rgba(0,0,0,0));}
+box-shadow:1px 1px 5px #27A5E7;color:#ffffff;border-radius:50px;" title="Join Now"><i></i> JOIN OUR TELEGRAM CHANNEL  <small><span class="telegramim_count" data-for="@KrishnaEarningZone">...</span> Subscriber</small></a> </center>
+
+ <script type="text/javascript">
+	atOptions = {
+		'key' : ' ',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://biblesausage.com/52cd619082dc5946ed214c6670f3cad9/invoke.js"></scr' + 'ipt>');
+</script>
+      <iframe allowtransparency="true" scrolling="no" frameborder="0" framespacing="0" width="320" height="10" src="about:blank"></iframe> 
+     </center> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Shivang Gupta</title>
+	<meta name="viewport" content="width=device-width">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+</head>
+<style>
+
+body{ font-family:Oleo Script; background:black; //background:radial-gradient(white,purple); } div { margin: 5px; background-color: none; border: solid; color: black; padding: 15px 32px; text-align: center; }hr { overflow: visible; /* For IE */ height: 3px; border-style: solid; border-color: blue; border-width: 2px 0 0 0; border-radius: 2px; }hr:before { /* Not really supposed to work, but does */ display: block; content: ""; height: 3px; border-style: solid; border-color: blue; border-width: 0 0 2px 0; border-radius: 2px; }.script {color:blue;text-decoration:none; } .script:hover{color:green;text-decoration:none;} table{ width:100%px; table-layout: fixed;}.tbl-header{ background-color: rgba(255,255,255,0.3); }.tbl-content{ height:300px; overflow-x:auto; margin-top: 0px; border: 2px solid rgba(255,255,255,0.3);}th{ padding: 20px 15px; text-align: left; font-weight: 500; font-size: 15px; color: darkred; }td{ padding: 15px; text-align: left; vertical-align:middle; font-weight: 300; font-size: 17px; color: #fff; border-bottom: solid 1px rgba(255,255,255,0.1);} /* demo styles */ @import url(https://fonts.googleapis.com/css?family=Roboto:400,500,300,700);body{ background: -webkit-linear-gradient(left, #25c481, #25b7c4); background: linear-gradient(to right, #25c481, #25b7c4); font-family: Oleo Script;}section{ margin: 10px;} /* for custom scrollbar for webkit browser*/ ::-webkit-scrollbar { width: 18px;} ::-webkit-scrollbar-track { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } ::-webkit-scrollbar-thumb { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } input, textarea, select { display: inline-block; -webkit-box-sizing: content-box; -moz-box-sizing: content-box; box-sizing: content-box; padding: 10px 20px; border: 1px solid #b7b7b7; -webkit-border-radius: 13px / 21px; border-radius: 13px / 21px; font: normal 16px/normal "Comic Sans MS", cursive, sans-serif; color: rgba(0,142,198,1); text-align: center; -o-text-overflow: clip; text-overflow: clip; word-spacing: 1px; -webkit-box-shadow: 1px 0 12px 5px rgba(58,110,201,0.2) ; box-shadow: 1px 0 12px 5px rgba(58,110,201,0.2) ; -webkit-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); -moz-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); -o-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1); min-width: 20%; } input[type=text], select { width: 80%; background-color: white;font-family: normal;color: rgba(0,142,198,1); font-weight:normail; padding: 12px 20px; margin: 5px 0; display: inline-block; border: 2px solid #00aaff; border-radius: 7px; box-shadow: 1px 4px 3px #aaa; box-sizing: border-box;} input[type=text]:hover {width: 85%; background-color:white;border: 2px  solid black;} fieldset { padding:1px 2px; } input[type="submit"] {color: white; font-weight: bold;width: 40%; padding: 13px 16px; background: #00aaff; border: none;} input[type=submit]:hover { 	background-color : #262626; } input, textarea, select { background: #f4f4f4; width: 70%; margin-top: 2px; font-family: Alura;margin-bottom: 2px; font: ; border: 1px solid #00aaff; border-radius: 9px; padding: 13px 16px; color: #000;}.head{text-shadow: #e6daac 1px 1px;color: #444;font-size: 18px;font-weight: bold; padding: 3px;border-bottom: 1px solid #edaa09; background: #fff2bf; box-shadow: 1px 2px 3px #aaa;}.header{color: #fff;font-weight: bold; padding: 1px 2px; border: 2px solid red; background: #fff;background-img: url(http://css-style.xtgem.com/image/bmenu.gif) repeat-x top; box-shadow: 1px 1px 2px #aaa;border-radius: 5px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;}
+.title{color: #fff;font-weight: bold; padding: 20px; }.error {background:#ffebe8; border:1px solid #dd3c10; padding:2px; text-align:center;font-weight:bold; padding: 8px 10px;color:#000; border-radius: 7px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;} .success {background:#fff8cc; border:2px solid #ffe222; padding:10px; text-align:center; font-weight:bold; padding: 8px 10px;color:#000;border-radius: 7px; box-shadow: 1px 2px 2px #aaa; box-sizing: border-box;}.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }.spd{background-color: #000; background-img: url() no-repeat; padding: 5px 7px; margin: 2px; border: 1px dotted darkred;}.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; } Zig { 	border-radius : 5px; 	background-color : #0AFF0A; 	padding : 20px; } div { 	border-radius : 5px; 	background-color :lighterd; 	padding : 20px;border:2px solid blue; padding:20px; color:#000; background-img: url() no-repeat; padding: 5px 7px; margin: 2px; }.sp{ background: url() no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }.DV{ 	background:#ffebe8; border:1px solid #dd3c10; padding:2px; text-align:center; font-weight:normal; color:maroon; } body{background:linear-gradient( to right black 100%); font-family:Oleo Script; color:black; opacity:100%; }.non{display:none;},
+</style>
+
+<body><center><font size='5'><b>Quixo App Refer Script</b></font></center></div>
+<fieldset style='border: 1px  ;'><center><font color='#000000'><b>Shivang Gupta</b></center></font><hr>
+<font color='#000' size='px'></font></center></fieldset>
+
+<fieldset style='border: 4px  '>
+<div>
+<div>
+
+<form action='bypass.php' method='get'>
+<input type='text' name='refer' placeholder='ENTER REFER CODE' class='text'><br><br><center>
+<input type='submit' value='SUBMIT' name='SUBMIT' class='submit'>
+  </center> 
+
+<center><script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//telegram.im/widget-button/index.php?id=@KrishnaEarningZone";document.getElementsByTagName("head")[0].appendChild(script);})();</script> <a href="tg://resolve?domain=KrishnaEarningZone" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:18px;max-width:350px;background: linear-gradient(20deg,  #40bfbf 30%, #ff0000 50%,#0066ff 30%
+ );text-transform:uppercase;background-size: 400% 400%; animation: gradient 2s ease infinite; } @keyframes gradient { 0% { background-position: 0% 50%; } 50% { background-position: 100% 50%; } 100% { background-position: 0% 50%; }}
+hr{
+    border:0;
+    height:1px;
+    background-image:linear-gradient(to right,rgba(0,0,0,0),rgba(0,255,0,1),rgba(0,0,0,0));}
+box-shadow:1px 1px 5px #27A5E7;color:#ffffff;border-radius:50px;" title="Join Now"><i></i> JOIN OUR TELEGRAM CHANNEL  <small><span class="telegramim_count" data-for="@KrishnaEarningZone">...</span> Subscriber</small></a> </center>
+<center><script type="text/javascript">
+	atOptions = {
+		'key' : ' ',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://biblesausage.com/52cd619082dc5946ed214c6670f3cad9/invoke.js"></scr' + 'ipt>');
+</script>
+      <iframe allowtransparency="true" scrolling="no" frameborder="0" framespacing="0" width="320" height="10" src="about:blank"></iframe> 
+</center>
+</body>
+</html>
